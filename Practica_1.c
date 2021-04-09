18994420bb40e66a04cee77922f212c892dd7ea5
lorenzoog00 <82126374+lorenzoog00@users.noreply.github.com>
2021-04-08 19:59:08 -0500

    Update Practica_1.c
    se corrigio el bugg que impedia que el while saliera. Se me estaban haciendo unos loops infinitos.

--- Practica_1.c
+++ Practica_1.c
@@ -4,10 +4,11 @@ void Menu(int *problema);
 
 
 int main(void){
-  int problema;
-  Intro();
-  Menu(&problema);
- 
+int problema;
+Intro();
+Menu(&problema);
+Problemas(problema);
+return 0;
 }
 void Intro()
 {
@@ -23,6 +24,7 @@ void Menu(int *problema)
   printf("Qué problema desea utilizar, de querer terminar el programa escribir 3\n" );
   scanf("%d" ,problema); 
 }
+
 void Problemas (int problema)
   {
     int lim_inf,lim_sup,flag,acomulado,flag2,n,enter,num,impresion; //n es un contador de limite inferior +1
@@ -31,7 +33,7 @@ void Problemas (int problema)
       {
 	if(problema==1)
 	  {
-	   
+        //printf("Entraste al problema 1\n");
 	    lim_inf=1;
 	    printf("Qué número quieres?\n");
 	    scanf( "%d", &num);
@@ -39,37 +41,54 @@ void Problemas (int problema)
 	      {
 		while(lim_inf<num/2)
 		  {
+            //printf("Entraste al while \n");
 		    acomulado=0;
 		    flag=0;
 		    impresion=lim_inf;
 		    n=lim_inf+1;
 		    lim_sup=lim_inf+2;
 		    flag2=0;
+            //printf("Impresion: %d \n", impresion);
 		    do{	
 		      if(flag==0)
+              
 			{
 			  acomulado=lim_inf+n+lim_sup;
+              //printf("Acumulado: %d \n", acomulado);
 			  flag=1;
 			}
 		      if(acomulado==num)
 			{
-			  
+              
+			  //printf("Acumulado 2: %d \n", acomulado);
 			  while(impresion<=lim_sup)
 			    {
-			      printf("%d + ", impresion);
-			      impresion++;
-			      flag2=1;
+			      
+                  if (impresion == lim_sup)
+                  {
+                    printf("%d", impresion);
+			        impresion++;
+			        flag2=1;
+                  }else{
+                    printf("%d + ", impresion);
+			        impresion++;
+			        flag2=1;
+                  }
+                  
+                  
 			    }
+                printf(" = %d\n", num);
 			}
-		      if(flag2==0)
-			{
+		      //if(flag2==0)
+			
+            //{
 			  lim_sup++;
 			  acomulado=acomulado+lim_sup;
 			  
-			}
+			//}
 		    }
 		    while(acomulado<=num);
-		    printf("papa");
+		    //printf("papa");
 		    lim_inf++;
 		  }
 	      }
@@ -77,6 +96,10 @@ void Problemas (int problema)
 	      {
 		printf("Introdujiste un valor erroneo, favor de presionar ENTER para volver al menú\n");
 		scanf("%d", &enter);
+        //Menu(&enter);
 	      } 
 	  }
+      }
 
+
+  }//end of problema
